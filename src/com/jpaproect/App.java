f7a83a28925e3bc0359ae671e4ddca3b3f95e6aa
Abel Cumbane <abelrosac@gmail.com>
2025-02-21 10:00:44 +0100

    chore: consultas com JPQL

--- src/com/jpaproect/App.java
+++ src/com/jpaproect/App.java
@@ -1,8 +1,11 @@
 package com.jpaproect;
 
+import java.util.List;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
 
 public class App {
 	
@@ -23,12 +26,34 @@ public class App {
 		//Person p = entityManager.find(Person.class, 2);
 		//System.out.println(p);
 		
-		Person p = entityManager.find(Person.class, 2);
-		entityManager.remove(p);
+		//Faz a remocao dos dados que estao na tabela person na BD:
+		//Person p = entityManager.find(Person.class, 2);
+		//entityManager.remove(p);
 
+		/*
+		Person p = new Person("Abel",25);
+		Person p1 = new Person("Einsten",9);
+		Person p2 = new Person("Zyarie",4);
+		Person p3 = new Person("Keiton",9);
+		Person p4 = new Person("Jade",1);
 		
-		//entityManager.persist(p);
+		
+		entityManager.persist(p);
+		entityManager.persist(p1);
+		entityManager.persist(p2);
+		entityManager.persist(p3);
+		entityManager.persist(p4);
+
 		entityManager.getTransaction().commit();
+		*/
+		
+		//JPQL statement
+		Query query = entityManager.createQuery("SELECT p FROM Person p WHERE p.name LIKE '%el'");
+		List<Person> people = (List<Person>) query.getResultList();
+		
+		for(Person p : people) {
+			System.out.println(p);
+		}
 		
 		entityManager.close();
 		factory.close();
