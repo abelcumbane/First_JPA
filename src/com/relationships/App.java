61df13ee45047ba5aba82df668fb6791069cf33f
Abel Cumbane <abelrosac@gmail.com>
2025-02-25 04:27:49 +0100

    chore: Inicializacao

--- src/com/relationships/App.java
+++ src/com/relationships/App.java
@@ -1,5 +1,37 @@
 package com.relationships;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class App {
+	
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("com.jpaproect");
+		EntityManager entityManager = factory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		
+		Address address = new Address();
+		address.setStreetName("Rua 4, Maputo");
+		address.setZipCode(144);
+		
+		
+		Employee employee = new Employee();
+		employee.setEmployeeName("Abel");
+		employee.setAddress(address);
+		
+		address.setEmployee(employee);
+		
+		entityManager.persist(employee);
+		entityManager.persist(address);
+		
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		factory.close();
+		
+	}
 
 }
