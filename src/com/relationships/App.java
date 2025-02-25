27e02c3d7624c0bf7d7acb4de43987b0d3589b64
Abel Cumbane <abelrosac@gmail.com>
2025-02-25 13:34:41 +0100

    chore: Inicializacao da ManyToMany

--- src/com/relationships/App.java
+++ src/com/relationships/App.java
@@ -13,6 +13,28 @@ public class App {
 		
 		entityManager.getTransaction().begin();
 		
+		Professor prof1 = new Professor("Abel Cumbane");
+		Professor prof2 = new Professor("Beto Abel");
+		
+		ResearchProject project1 = new ResearchProject("Projecto de Reabilitar casas");
+		ResearchProject project2 = new ResearchProject("Projecto de Estudos Fisicos");
+		ResearchProject project3 = new ResearchProject("Projecto de Alimentacao");
+		
+		prof1.assinProjectToProfessor(project1);
+		prof1.assinProjectToProfessor(project2);
+		prof2.assinProjectToProfessor(project3);
+		prof2.assinProjectToProfessor(project2);
+		
+		project1.assinProfessorToProject(prof1);
+		project1.assinProfessorToProject(prof2);
+		project1.assinProfessorToProject(prof1);
+		project1.assinProfessorToProject(prof2);
+		
+		entityManager.persist(prof1);
+		entityManager.persist(prof2);
+		entityManager.persist(project1);
+		entityManager.persist(project2);
+		entityManager.persist(project3);
 		
 		
 		entityManager.getTransaction().commit();
